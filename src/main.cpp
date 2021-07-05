73bccff38023dd4765265944b2f5c65e739dc0c2
VeeDeeBee <rubenvandenberghe@gmail.com>
2021-07-05 22:31:39 +0200

    Basic Oled Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display(-1);
 
 void setup() {
-  // put your setup code here, to run once:
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  
+
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+
+  display.println("Welcome!");
+  display.display();
+  delay(2000);
 }
 
 void loop() {
